e5c363a9feae2de2901213ad48e1bebe29e0c8f3
Bryan <le__bryan@hotmail.com>
2020-10-30 17:46:05 -0500

    Modified run.py to get the images_file name

--- run.py
+++ run.py
@@ -6,15 +6,20 @@ import requests
 #Data for the dict: name, weight(int), description, image_name.
 files_path = '/home/bryan/Documents/text_files/'
 text_files = os.listdir('/home/bryan/Documents/text_files/')
+#List to iterate and assign the matching image
+#image_directory = os.listdir('/home/bryan/Documents/')
 data_dict = {}
+#i = 0
 for text in text_files:
-    with open(files_path+text, 'r') as file:
-        content = file.readlines()
+    with open(files_path+text, 'r') as file:        
+        content = file.readlines()        
+        
         fruit_weight = int(content[1].split(' ')[0])
         data_dict['name'] = content[0]        
         data_dict['weight'] = fruit_weight
         data_dict['description'] = content[2]
-        data_dict['image_name'] = "Working on it"
+        #data_dict['image_name'] = image_directory[i] # i += 1
+
     print(data_dict)
         
 
